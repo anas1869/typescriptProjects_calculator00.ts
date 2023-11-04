e3e7793eeeb12e5c9eca63a13a9767e52940f0af
anas1869 <ismartwinner49@gmail.com>
2023-11-04 00:29:35 -0700

    Initial commit

---
+++ wmd/project00/index.ts
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+
+let {num1,num2,operation} = await inquirer.prompt([
+{
+    name:"num1",
+    type:"number",
+    message:"please enter your first number",
+},{
+    name:"num2",
+    type:"number",
+    message:"please enter your second number",
+},{
+    
+name:"operation",
+type:"list",
+choices:["addition","substraction","multiplication","division"],
+message:"please enter your age",
+}
+
+    
+]);
+
+// console.log(answers)
+
+if (operation === "addition"){
+    console.log(`the sum of two numbers = ${num1 + num2}`)
+}else if(operation === "substraction"){
+    console.log(`the diffrence between two numbers = ${num1-num2}`)
+} else if (operation==="multiplication"){
+    console.log(`The multiplication of two numbers = ${num1*num2}`)
+}else if(operation==="division") {
+    console.log(`The division between two numbers = ${num1/ num2}`)
+}
